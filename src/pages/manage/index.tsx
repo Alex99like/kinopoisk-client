4172de0329339cefa8aded42fb0c3c0517c7ab30
Alexander <aleksievisa@gmail.com>
2023-04-12 21:11:34 +0300

    feat: add interseptors

---
+++ src/pages/manage/index.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+
+const AdminPage: NextPage = () => {
+  return (
+    <div>Admin Page</div>
+  )
+}
+
+export default AdminPage
---
+++ src/pages/profile.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+
+const ProfilePage: NextPage = () => {
+  return <div>Profile Page</div>
+}
+
+export default ProfilePage
