6997e72d48e9c2ad017ffe5e59b6fc46a9336943
Alexander <aleksievisa@gmail.com>
2023-04-13 14:29:57 +0300

    feat: add genres panel admin

--- src/service/genre.service.ts
+++ src/service/genre.service.ts
@@ -1,15 +1,49 @@
 import { axiosClassic } from '@/api/interseptor'
+import { IGenreEditInput } from '@/components/screens/admin/genre/genre-edit.interface'
 import { getGenresUrl } from '@/config/api.config'
 import { IGenre } from '@/shared/types/movie.types'
 import axios from 'axios'
 
 export const GenreService = {
-  async getAll(searchTerm?: string) {
-    return axiosClassic.get<IGenre[]>(getGenresUrl(''), {
-      params: searchTerm 
-        ? {
-            searchTerm
-        } : {}
-    })
-  }
+  async getBySlug(slug: string) {
+		return axiosClassic.get<IGenre>(getGenresUrl(`/by-slug/${slug}`))
+	},
+
+	async create() {
+		return axios.post<string>(getGenresUrl(''))
+	},
+
+	async update(_id: string, data: IGenreEditInput) {
+		return axios.put<string>(getGenresUrl(`/${_id}`), data)
+	},
+
+	async delete(_id: string) {
+		return axios.delete<string>(getGenresUrl(`/${_id}`))
+	},
+
+	async getAll(searchTerm?: string) {
+		return axiosClassic.get<IGenre[]>(getGenresUrl(``), {
+			params: searchTerm
+				? {
+						searchTerm,
+				  }
+				: {},
+		})
+	},
+
+	// async getCollections() {
+	// 	return axiosClassic.get<ICollection[]>(getGenresUrl('/collections'))
+	// },
+
+	async getById(_id: string) {
+		return axios.get<IGenreEditInput>(getGenresUrl(`/${_id}`))
+	},
+
+	async getPopularGenres(limit: number = 4) {
+		return axiosClassic.get<IGenre[]>(getGenresUrl(`/popular`), {
+			params: {
+				limit,
+			},
+		})
+	},
 }
